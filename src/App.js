cf2bf289f01f377884b0591c238bdcd99a1b9216
Lê Thịnh <thinh.le@youthdev.net>
2019-11-18 10:28:21 +0700

    demo todo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React, { Component } from 'react'
+import CreateTodo from './containers/CreateTodo'
+import Table from './containers/Table'
 
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import Input from './component/input';
-function App() {
-  return (
-    <>
-    <Input></Input>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container" style={{ marginTop: "80px"}} >
+          <div className="row">
+            <div className="col-lg-10 offset-lg-2 col-md-10 col-sm-12 col-xs-12">
+              <CreateTodo />
+            </div>
+            <Table />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
 export default App;
